e2d4f93b12a7592b527d62136f9ecff764395b7b
github username <github email address>
2017-01-18 21:34:34 -0500

    my beautiful trading cards

--- js/card.js
+++ js/card.js
@@ -1,11 +1,15 @@
 $( function(){
 
   $( "#card-one").click(function(){
-    //Use toggleClass method here
+    $("#card-one").toggleClass("flipped")
   });
 
 
-	// code click event for card-two here
+	$( "#card-two").click(function(){
+    $("#card-two").toggleClass("flipped")
+  });
 
-  // code click event for card-three here
+  $( "#card-three").click(function(){
+    $("#card-three").toggleClass("flipped")
+  });
  })
